1e44a20c9e22c6f1ac44ca9181c1e8651f6ff28b
doomed-neko <79870712+doomed-neko@users.noreply.github.com>
2026-08-05 04:23:26 +0300

    improve usage info

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ A simple CLI interface for [LrcLib](https://lrclib.net) built with rust
 
 ```bash
 lrcli search -t <track_name> -a <artist> [-b album] [-d duration]
+Options:
+  -t, --track       Set the track name
+  -a, --artist      Set the artist name
+  -b, --album       Set the album name
+  -d, --duration    Set the duration (in seconds)
 ```
--- src/main.rs
+++ src/main.rs
@@ -63,6 +63,11 @@ fn print_usage_and_exit(program: &str) -> ! {
     println!("Find lyrics from https://lrclib.net");
     print!("Usage:");
     println!("\t{program} search -t <track_name> -a <artist> [-b album] [-d duration]");
+    println!("Options:");
+    println!("  -t, --track\t\tSet the track name");
+    println!("  -a, --artist\t\tSet the artist name");
+    println!("  -b, --album\t\tSet the album name");
+    println!("  -d, --duration\tSet the duration (in seconds)");
     exit(1)
 }
 
